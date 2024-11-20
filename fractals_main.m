22fbda4c6c8e370f6e8b41b96281bfb5ce240818
CharleyE14 <c.t.roberts@bham.ac.uk>
2024-11-20 16:54:06 +0000

    add < and > for multiplying and dividing the length of the line

--- fractals_main.m
+++ fractals_main.m
@@ -1,30 +1,68 @@
 disp("-----");
 clear;
 
+% these aren't always set, so define them here
+length_line = 1;
+length_scale = 1;
+
 %%%%%%%%%%%%%%%%%%%%%
 %{
 Examples:
 
+1)
 axiom = "F+F+F+F";
-grammar = "F+F-F-FF+F+F-F";
+grammar = {"F -> F+F-F-FF+F+F-F"};
 angle_delta = 90;
 iterations = 5;
 
+2)
 axiom = "F+F+F+F";
-grammar = "FF+F-F+F+FF";
+grammar = {"F -> FF+F-F+F+FF"};
 angle_delta = 90;
 iterations = 5;
 
-BRANCH:
+3) BRANCH:
 axiom = "F";
-grammar = "FF+[+F-F-F]-[-F+F+F]";
+grammar = {"F -> FF+[+F-F-F]-[-F+F+F]"};
 angle_delta = 22.5;
 iterations = 5;
 
+4)
+axiom = "VZFFF";
+grammar = {"V -> [+++W][---W]YV", "W -> +X[-W]Z", "X -> -W[+X]Z", "Y -> YZ", "Z -> [-FFF][+FFF]F"};
+angle_delta = 20;
+iterations = 10;
+
+5) test for > <
+axiom = "a"
+grammar = {'F -> >F<', 'a -> F[+x]Fb', 'b -> F[-y]Fa', 'x -> a', 'y -> b'}
+angle_delta = 45
+iterations = 5
+
+axiom = "a"
+grammar = {'F -> >F<', 'a -> F[+x]Fb', 'b -> F[-y]Fa', 'x -> a', 'y -> b'}
+angle_delta = 45
+iterations = 5
+
+--------------------------
+
+axiom = 
+grammar = 
+angle_delta = 
+iterations = 
+
 %}
+
+axiom = "a";
+grammar = {'F -> >F<', 'a -> F[+x]Fb', 'b -> F[-y]Fa', 'x -> a', 'y -> b'};
+angle_delta = 45;
+iterations = 15;
+length_scale = 1.36;
+
 %%%%%%%%%%%%%%%%%%%%%
 
 % constants
+line_width = 0.2; % width of the plotted lines
 origin = [5,5]; % the starting point of the plot
 full_rotation = 360; % in case of change to radians idk
 starting_angle = 0;
@@ -43,13 +81,6 @@ colours_ptr = 1;
 
 % stack
 stack = [];
-
-% inputs
-axiom = "X";
-grammar = {"F --> FF", "X --> F-[[X]+X]+F[+FX]-X"};
-angle_delta = 22.5;
-iterations = 6;
-unit_length = 1; % e.g. 1 means each F is 1 unit length
 
 %%%%%%%%%%%%%%%%%%%%%
 
@@ -75,10 +106,10 @@ for i = 1:size(grammar,2)
     r = char(r);
     r = r(~isspace(r));
 
-    % split into LHS and RHS, on "-->"
-    parts = split(r, "-->");
+    % split into LHS and RHS, on "->"
+    parts = split(r, "->");
     if size(parts,1) ~= 2
-        error("rule found with no or multiple '-->'");
+        error("rule found with no or multiple '->'");
     end
     gram_chars = [gram_chars; parts(1)];
     gram_rules = [gram_rules; parts(2)];
@@ -100,7 +131,7 @@ for i=1:iterations
     for n = 1:length(temp1)
         if (ismember(temp1(n),gram_chars)) % if the char read is a char in the grammar
             gram_index = find(strcmp(temp1(n), gram_chars));
-            temp2 = [temp2, gram_rules{gram_index}]; % replace all F with the full F-->..
+            temp2 = [temp2, gram_rules{gram_index}]; % replace all F with the full F->..
         else
             temp2 = [temp2, temp1(n)]; 
         end
@@ -118,17 +149,20 @@ for c = string
         % turn left
         curr_angle = mod(curr_angle-angle_delta,full_rotation);
     elseif c == 'F' % TODO don't hardcode the char here
-        [x,y,x_trail,y_trail] = move(curr_pos, curr_angle, unit_length, x_trail, y_trail);
-        curr_pos = [x,y];     
+        [x,y,x_trail,y_trail] = move(curr_pos, curr_angle, length_line, x_trail, y_trail);
+        curr_pos = [x,y];
     elseif c == '['
         stack = push(stack, curr_pos, curr_angle);
     elseif c == ']'
         [stack, curr_pos, curr_angle] = pop(stack);
-        plot(x_trail, y_trail, 'LineWidth', 1, 'Color', colour_main);
-        
+        plot(x_trail, y_trail, 'LineWidth', line_width, 'Color', colour_main);
+
         x_all = [x_all, x_trail]; y_all = [y_all, y_trail];
         x_trail = [curr_pos(1)]; y_trail = [curr_pos(2)];
-
+    elseif c == '<'
+        length_line = length_line / length_scale;
+    elseif c == '>'
+        length_line = length_line * length_scale;
     elseif ismember(c, gram_chars)
         % this is ok - just ignore - no drawing instructions from these
     else
@@ -141,7 +175,7 @@ end
 
 % if there's no push/pop then the whole thing will just be drawn at this
 % point
-plot(x_trail, y_trail, 'LineWidth', 1, 'Color', colour_main);
+plot(x_trail, y_trail, 'LineWidth', line_width, 'Color', colour_main);
 
 % some formating
 xticks([]);
@@ -155,9 +189,9 @@ x_padding = 0.1 * (max(x_all) - min(x_all));
 y_padding = 0.1 * (max(y_all) - min(y_all));
 
 % Set axis limits with padding
-axis([min(x_all) - x_padding, max(x_all) + x_padding, min(y_all) - y_padding, max(y_all) + y_padding]);
+%axis([min(x_all) - x_padding, max(x_all) + x_padding, min(y_all) - y_padding, max(y_all) + y_padding]);
 
-% export graphics#
+% export graphics
 disp("Exporting...");
 exportgraphics(gcf, 'temp.png', 'Resolution', 512);
 disp("Finished exporting!");
